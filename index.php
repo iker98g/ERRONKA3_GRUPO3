cb9b95255ed0235092d0936c12e21ebe343059bf
ikaslea <ikaslea@ETHAZI23>
2019-10-21 09:43:35 +0200

    vista de index (login) y arvhivos de estilos

--- index.php
+++ index.php
@@ -10,17 +10,28 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+        <form class="bg-secondary ">
+          <h1>LOGIN</h1>
+          <div class="form-group">
+            <input type="username" class="form-control" id="username" placeholder="Username"> 
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" id="password" aria-describedby="passHelp" placeholder="Password">
+            <small id="passHelp" class="form-text text-muted">We'll never share your password with anyone else.</small>
+          </div>
+          <div class="form-check card-body d-flex justify-content-between align-items-center">
+            <input type="checkbox" class="form-check-input" id="rememberCheck">
+            <label class="form-check-label" for="rememberCheck">Remember</label>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </div>
+          
+        </form>
+      </div>
 
-
-
-
-
-
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="view/js/script.js" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <!--<script src="view/js/script.js" type="text/javascript"></script>-->
 </body>
 </html>
