337c58cf33587138d05540cdf634c0391f6f103b
dwebxabier <iwebprueba2@gmail.com>
2019-10-21 13:50:43 +0200

    CUsers terminado

--- controller/CUsers.php
+++ controller/CUsers.php
@@ -5,8 +5,16 @@ include_once ("../model/usuario_model.php");
 $user= new usuario_model();
 $user->setList();
 
-$listaUsersJSON=$user->getListJsonString(); //attributes PRIVATEs or PROTECTED
-
-echo $listaUsersJSON;
-
-unset ($user);
+foreach ($user->list as $object){
+    if($object->getUsuario()==$_POST["usuario"] && $object->getContrasena()==$_POST["contrasena"]){
+        session_start();       
+        $_SESSION["usuario"] = $_POST["usuario"];
+        
+        if($object->getAdmin()==1){
+            header("Location: CAdmin.php");
+        }else {
+            header("Location: CHotel.php");
+        }
+    }
+}
+?>
