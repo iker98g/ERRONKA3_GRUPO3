9ea5ad4777ba35624514c3ed54c44044f650bfd9
iker <iker98g@gmail.com>
2019-10-22 14:30:19 +0200

    vista hotel.php sin acabar

--- view/hotel.php
+++ view/hotel.php
@@ -6,15 +6,117 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hoteles LES</title>
     <script src="https://kit.fontawesome.com/661afcc94b.js" crossorigin="anonymous"></script>
-    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="css/habitaciones.css" rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    <!-- NAV -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Hoteles LES</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="dropdown my-2 my-lg-0">
+                <button class="btn btn-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    alopez
+                    <img src="img/cara.PNG" width="30" height="30" alt="">
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-euro-sign"></i>    
+                        Moneda
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <div class="custom-control custom-switch">
+                            <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                            <label class="custom-control-label" for="customSwitch1">Modo nocturno</label>
+                        </div>
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-sign-out-alt"></i>    
+                        Cerrar sesión
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- NAV -->
+    <!-- CAROUSEL -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/hotel2.jpg" class="d-block w-100" height="450px" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/hotel3.jpg" class="d-block w-100" height="450px" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/hotel4.jpg" class="d-block w-100" height="450px" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/hotel5.jpg" class="d-block w-100" height="450px" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/hotel6.jpg" class="d-block w-100" height="450px" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/hotel7.jpg" class="d-block w-100" height="450px" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- CAROUSEL -->
+    <!-- LIST GROUP -->
+    <div class="row">
+        <div class="col-2">
+            <div class="list-group" id="list-tab" role="tablist">
+                <a class="list-group-item list-group-item-action active" id="list-descripcion-list" data-toggle="list" href="#list-descripcion" role="tab" aria-controls="descripcion">Descripción</a>
+                <a class="list-group-item list-group-item-action" id="list-habitaciones-list" data-toggle="list" href="#list-habitaciones" role="tab" aria-controls="habitaciones">Habitaciones</a>
+                <a class="list-group-item list-group-item-action" id="list-ubicacion-list" data-toggle="list" href="#list-ubicacion" role="tab" aria-controls="ubicacion">Ubicación</a>
+                <a class="list-group-item list-group-item-action" id="list-precios-list" data-toggle="list" href="#list-precios" role="tab" aria-controls="precios">Precios</a>
+                <a class="list-group-item list-group-item-action" id="list-contacto-list" data-toggle="list" href="#list-contacto" role="tab" aria-controls="contacto">Contacto</a>
+            </div>
+        </div>
+        <div class="col-10">
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="list-descripcion" role="tabpanel" aria-labelledby="list-descripcion-list">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, ratione. Laboriosam sequi vel voluptatem quis, quos quaerat iste, a deserunt alias eligendi nobis! Libero deleniti maiores, corrupti dolorum vel quod sapiente fuga, quos voluptatem voluptatibus, facere labore tempora quidem nam quae nulla minima cumque! Voluptatem, porro. Odio consequatur id expedita, ab nam nulla vitae dolore sit! Exercitationem cupiditate quaerat voluptatibus reiciendis saepe illum? Ipsum, quam! Cum provident doloribus alias ducimus architecto cumque, error animi dolores nisi quas officiis impedit soluta ad sint excepturi! Labore facere, nobis cumque placeat doloribus quis repudiandae saepe expedita. Numquam commodi possimus, iste perspiciatis, vero deserunt ipsum placeat in odio, facilis repudiandae dolor consequatur. Beatae, placeat ad? Consequatur aut voluptatem nemo temporibus. Minus, hic consequatur velit nisi numquam necessitatibus voluptatibus error deserunt laboriosam eligendi eum nulla iusto inventore nobis accusantium perferendis laborum aliquam nesciunt nostrum reprehenderit molestiae ea dolorum quia in. Deserunt, excepturi ex quia eum nostrum incidunt ipsum voluptatum eius veniam laboriosam, sapiente doloremque, molestias repellendus ut veritatis sunt? Facere quod molestias aperiam debitis corrupti. Accusantium consectetur atque, nihil voluptates tempora nam temporibus voluptas velit debitis modi, in suscipit maxime, inventore aliquid ratione harum blanditiis id. Quis adipisci recusandae, error cupiditate aliquam quisquam eos saepe expedita asperiores qui cumque eum id pariatur illum voluptates veritatis assumenda necessitatibus deleniti iusto non nesciunt perspiciatis placeat ex magnam. Molestiae est tenetur doloremque velit modi veniam nemo molestias, iste dolorum error, vero, vitae voluptates. Eos dolore doloremque, nemo voluptatum dignissimos fuga, id delectus magni illum eum laudantium placeat aliquid ducimus. Cum qui a suscipit? Voluptatem molestiae voluptate quasi ea quod, sequi veritatis, quos eveniet culpa nulla id modi hic quia sit deleniti, unde atque iusto eligendi rerum aperiam impedit error? Vero accusamus in asperiores consequatur aperiam sunt tempora doloribus, quos dolorum fugit autem voluptatum omnis dolores numquam mollitia hic?</div>
+                <div class="tab-pane fade" id="list-habitaciones" role="tabpanel" aria-labelledby="list-habitaciones-list">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                <!-- AJAX AQUI -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="list-ubicacion" role="tabpanel" aria-labelledby="list-ubicacion-list">...</div>
+                <div class="tab-pane fade" id="list-precios" role="tabpanel" aria-labelledby="list-precios-list">...</div>
+                <div class="tab-pane fade" id="list-contacto" role="tabpanel" aria-labelledby="list-contacto-list">...</div>
+            </div>
+        </div>
+    </div>
+    <!-- LIST GROUP -->
 
-
-
-
+        
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="js/script.js" type="text/javascript"></script>
---
+++ view/js/hotel.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+	
+	$.ajax({
+		type:"GET",
+		url: "../controller/CHabitaciones.php", 
+	 	datatype: "json",  // type of the result
+		
+	 	success: function(result){  
+			
+			var habitaciones = JSON.parse(result);
+			
+			console.log(result);
+						
+			var newRow="";
+			
+			$.each(habitaciones,function(index,room){
+				newRow += '<img src="'+room.imagen+'" class="card-img-top" alt="..."></img>'
+							+"<h5>"+room.tipo+" class='card-title'</h5>"
+							+"<button type='button' class='btn btn-primary></button>"
+			});
+			$(".card-body").append(newRow);
+			
+	  },
+		error : function(xhr) {
+			alert("An error occured: " + xhr.status + " " + xhr.statusText);
+		}
+   });
+});
