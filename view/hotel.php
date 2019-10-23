421b182285581e5dd4b9c747b3dd59290df07eeb
iker <iker98g@gmail.com>
2019-10-23 14:50:25 +0200

    hotel.php sin acabar

--- view/hotel.php
+++ view/hotel.php
@@ -4,45 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hoteles LES</title>
+    <title>Hotel LES</title>
     <script src="https://kit.fontawesome.com/661afcc94b.js" crossorigin="anonymous"></script>
-    <link href="css/habitaciones.css" rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/habitaciones.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
+<!-- HEADER -->
+<header> 
     <!-- NAV -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Hoteles LES</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <div class="dropdown my-2 my-lg-0">
-                <button class="btn btn-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    alopez
-                    <img src="img/cara.PNG" width="30" height="30" alt="">
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">
-                        <i class="fas fa-euro-sign"></i>    
-                        Moneda
-                    </a>
-                    <a class="dropdown-item" href="#">
-                        <div class="custom-control custom-switch">
-                            <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                            <label class="custom-control-label" for="customSwitch1">Modo nocturno</label>
-                        </div>
-                    </a>
-                    <a class="dropdown-item" href="#">
-                        <i class="fas fa-sign-out-alt"></i>    
-                        Cerrar sesión
-                    </a>
-                </div>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Hotel LES</a>
+        <div class="dropdown dropleft">
+            <button class="btn btn-light" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                alopez
+                <img src="img/cara.PNG" width="30" height="30" alt="">
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">
+                    <i class="fas fa-euro-sign"></i>    
+                    Moneda
+                </a>
+                <a class="dropdown-item" href="#">
+                    <div class="custom-control custom-switch">
+                        <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                        <label class="custom-control-label" for="customSwitch1">Modo nocturno</label>
+                    </div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                    <i class="fas fa-sign-out-alt"></i>    
+                    Cerrar sesión
+                </a>
             </div>
         </div>
     </nav>
-    <!-- NAV -->
+    <!-- FIN NAV -->
+</header> 
+<!-- FIN HEADER -->     
+<!-- MAIN -->
+<main>
     <!-- CAROUSEL -->
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
@@ -82,42 +83,175 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
-    <!-- CAROUSEL -->
+    <!-- FIN CAROUSEL -->
     <!-- LIST GROUP -->
-    <div class="row">
-        <div class="col-2">
+    <div class="row separacion">
+        <div class="col-4">
             <div class="list-group" id="list-tab" role="tablist">
                 <a class="list-group-item list-group-item-action active" id="list-descripcion-list" data-toggle="list" href="#list-descripcion" role="tab" aria-controls="descripcion">Descripción</a>
                 <a class="list-group-item list-group-item-action" id="list-habitaciones-list" data-toggle="list" href="#list-habitaciones" role="tab" aria-controls="habitaciones">Habitaciones</a>
                 <a class="list-group-item list-group-item-action" id="list-ubicacion-list" data-toggle="list" href="#list-ubicacion" role="tab" aria-controls="ubicacion">Ubicación</a>
-                <a class="list-group-item list-group-item-action" id="list-precios-list" data-toggle="list" href="#list-precios" role="tab" aria-controls="precios">Precios</a>
                 <a class="list-group-item list-group-item-action" id="list-contacto-list" data-toggle="list" href="#list-contacto" role="tab" aria-controls="contacto">Contacto</a>
             </div>
         </div>
-        <div class="col-10">
+        <div class="col-8">
             <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="list-descripcion" role="tabpanel" aria-labelledby="list-descripcion-list">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, ratione. Laboriosam sequi vel voluptatem quis, quos quaerat iste, a deserunt alias eligendi nobis! Libero deleniti maiores, corrupti dolorum vel quod sapiente fuga, quos voluptatem voluptatibus, facere labore tempora quidem nam quae nulla minima cumque! Voluptatem, porro. Odio consequatur id expedita, ab nam nulla vitae dolore sit! Exercitationem cupiditate quaerat voluptatibus reiciendis saepe illum? Ipsum, quam! Cum provident doloribus alias ducimus architecto cumque, error animi dolores nisi quas officiis impedit soluta ad sint excepturi! Labore facere, nobis cumque placeat doloribus quis repudiandae saepe expedita. Numquam commodi possimus, iste perspiciatis, vero deserunt ipsum placeat in odio, facilis repudiandae dolor consequatur. Beatae, placeat ad? Consequatur aut voluptatem nemo temporibus. Minus, hic consequatur velit nisi numquam necessitatibus voluptatibus error deserunt laboriosam eligendi eum nulla iusto inventore nobis accusantium perferendis laborum aliquam nesciunt nostrum reprehenderit molestiae ea dolorum quia in. Deserunt, excepturi ex quia eum nostrum incidunt ipsum voluptatum eius veniam laboriosam, sapiente doloremque, molestias repellendus ut veritatis sunt? Facere quod molestias aperiam debitis corrupti. Accusantium consectetur atque, nihil voluptates tempora nam temporibus voluptas velit debitis modi, in suscipit maxime, inventore aliquid ratione harum blanditiis id. Quis adipisci recusandae, error cupiditate aliquam quisquam eos saepe expedita asperiores qui cumque eum id pariatur illum voluptates veritatis assumenda necessitatibus deleniti iusto non nesciunt perspiciatis placeat ex magnam. Molestiae est tenetur doloremque velit modi veniam nemo molestias, iste dolorum error, vero, vitae voluptates. Eos dolore doloremque, nemo voluptatum dignissimos fuga, id delectus magni illum eum laudantium placeat aliquid ducimus. Cum qui a suscipit? Voluptatem molestiae voluptate quasi ea quod, sequi veritatis, quos eveniet culpa nulla id modi hic quia sit deleniti, unde atque iusto eligendi rerum aperiam impedit error? Vero accusamus in asperiores consequatur aperiam sunt tempora doloribus, quos dolorum fugit autem voluptatum omnis dolores numquam mollitia hic?</div>
+                <div class="tab-pane fade show active" id="list-descripcion" role="tabpanel" aria-labelledby="list-descripcion-list">
+                    <h5>¿Quienes somos?</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In illo magnam dolorem reprehenderit, laboriosam commodi magni voluptatibus quasi ullam veritatis? Maiores dicta, laborum natus, voluptas laudantium reiciendis mollitia ipsa cumque commodi vel tempore. Maxime, facilis aliquam quasi, unde fugiat ipsum et voluptates soluta vel obcaecati quibusdam eveniet accusamus doloremque minus optio harum architecto temporibus in exercitationem sunt vitae provident aliquid? A, hic. Voluptas enim rerum ipsam minus vel facere aspernatur corrupti officiis voluptatum perspiciatis a, tempore porro quia. Quisquam earum, ab quis quaerat pariatur, dolorem est qui ut maxime voluptatem eius rem. Veniam neque cupiditate natus nulla repudiandae ratione, provident eligendi, ad eaque obcaecati, pariatur atque ex animi voluptas illo. Saepe voluptatum, earum recusandae quae eos cum dolor voluptates, porro iure consequatur quo! Aperiam aspernatur modi labore, iste consectetur quas velit unde alias reiciendis! Ad perspiciatis incidunt sit sint ipsam neque velit quisquam, quis voluptatem. Facere mollitia sed repellendus, quae eum adipisci veniam dolores ex? Delectus sequi, tempore voluptate non fugiat magni quasi fuga, enim porro consectetur incidunt aut autem assumenda culpa doloremque repudiandae accusamus. Eos velit harum deleniti consectetur. Neque corrupti asperiores accusantium? Iusto quasi, fugiat aliquam placeat ad corrupti accusantium suscipit deserunt quia, quos cum maiores a animi?</p>
+                </div>
                 <div class="tab-pane fade" id="list-habitaciones" role="tabpanel" aria-labelledby="list-habitaciones-list">
+                    <h5>Habitaciones</h5>
                     <div class="row">
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <div class="card">
                                 <div class="card-body">
-                                <!-- AJAX AQUI -->
+                                    <img src="img/estandar1.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Estandar</h5>
+                                    <p class="card-text">65€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/estandar2.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Estandar</h5>
+                                    <p class="card-text">65€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/estandar3.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Estandar</h5>
+                                    <p class="card-text">65€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/estandar4.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Estandar</h5>
+                                    <p class="card-text">65€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/suite1.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Suite</h5>
+                                    <p class="card-text">90€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/suite2.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Suite</h5>
+                                    <p class="card-text">90€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/suite3.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Suite</h5>
+                                    <p class="card-text">90€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/suite4.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Suite</h5>
+                                    <p class="card-text">90€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/superior1.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Superior</h5>
+                                    <p class="card-text">75€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/superior2.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Superior</h5>
+                                    <p class="card-text">75€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/superior3.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Superior</h5>
+                                    <p class="card-text">75€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="img/superior4.jpg" class="card-img-top" alt="...">
+                                    <h5 class="card-title">Superior</h5>
+                                    <p class="card-text">75€</p>
+                                    <a href="#" class="btn btn-primary">Reservar</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="tab-pane fade" id="list-ubicacion" role="tabpanel" aria-labelledby="list-ubicacion-list">...</div>
-                <div class="tab-pane fade" id="list-precios" role="tabpanel" aria-labelledby="list-precios-list">...</div>
-                <div class="tab-pane fade" id="list-contacto" role="tabpanel" aria-labelledby="list-contacto-list">...</div>
+                <div class="tab-pane fade" id="list-ubicacion" role="tabpanel" aria-labelledby="list-ubicacion-list">
+                    <h5>Ubicación</h5>
+                    <iframe class="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.3889090433854!2d-2.925636384553234!3d43.26422757913655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e4fc5e5c211dd%3A0x6a7486dfa11cb52e!2sAyuntamiento%20de%20Bilbao!5e0!3m2!1ses!2ses!4v1571815421752!5m2!1ses!2ses" width="400" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+                <div class="tab-pane fade" id="list-contacto" role="tabpanel" aria-labelledby="list-contacto-list">
+                    <h5>Contáctanos</h5>
+                    <p>¿Tienes preguntas o quieres saber más sobre cómo reservar habitaciones? Contacta con nosotros a través de correo electrónico o teléfono.</p>
+                    <p>Correo electrónico: hotel_les@gmail.com</p>
+                    <p>Teléfono: 944581043</p>      
+                </div>
             </div>
         </div>
     </div>
-    <!-- LIST GROUP -->
-
-        
-
+    <!-- FIN LIST GROUP -->
+</main>    
+<!-- FIN MAIN -->    
+<!-- FOOTER -->
+<footer class="text-muted text-center text-small">
+    <p class="mb-1">&copy; Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dignissimos dolores fuga accusantium iste quisquam explicabo atque praesentium voluptas eaque deleniti, maxime tempora et? Rerum placeat voluptate, maxime sequi itaque vel est totam neque voluptas soluta hic reprehenderit laborum doloribus ut iure sapiente quos blanditiis accusantium ullam? Temporibus, vitae mollitia.</p>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a href="https://www.instagram.com/?hl=es"><i class="fab fa-instagram"></i></a></li>
+        <li class="list-inline-item"><a href="https://es-es.facebook.com/"><i class="fab fa-facebook-square"></i></a></li>
+        <li class="list-inline-item"><a href="https://twitter.com/?lang=es"><i class="fab fa-twitter"></i></a></li>
+        <li class="list-inline-item"><a href="https://support.google.com/plus/?hl=es-419#topic=9259565"><i class="fab fa-google-plus-g"></i></a></li>
+    </ul>
+</footer>
+<!-- FIN FOOTER -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="js/script.js" type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
