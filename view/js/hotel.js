2d89106677c5a06e0f3c466e8cfaca937794a177
iker <iker98g@gmail.com>
2019-10-24 14:26:59 +0200

    connect_data modificado

--- view/js/hotel.js
+++ view/js/hotel.js
@@ -1,29 +1,15 @@
 $( document ).ready(function() {
 	
+	//ajax habitaciones
 	$.ajax({
 		type:"GET",
-		url: "../controller/CHabitaciones.php", 
-	 	datatype: "json",  // type of the result
-		
-	 	success: function(result){  
-			
-			var habitaciones = JSON.parse(result);
-			
+		url:"../controller/CHabitacionesList.php",
+		datatype:"json",
+
+		success:function(result){
+			alert(result);
+			var habitaciones=JSON.parse(result);
 			console.log(result);
-						
-			var newRow="";
-			
-			$.each(habitaciones,function(index,room){
-				newRow += '<img src="'+room.imagen+'" class="card-img-top" alt="..."></img>'
-							+"<h5>"+room.tipo+" class='card-title'</h5>"
-							+"<p>"+room.precio+" class='card-text'</h5>"
-							+"<button type='button' class='btn btn-primary></button>"
-			});
-			$(".card-body").append(newRow);
-			
-	  },
-		error : function(xhr) {
-			alert("An error occured: " + xhr.status + " " + xhr.statusText);
 		}
-   });
+	})
 });
