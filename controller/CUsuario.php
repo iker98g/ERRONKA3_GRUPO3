37dd6b87bd69f162dbf9dbaa85750f12dc50ac25
dwebxabier <iwebprueba2@gmail.com>
2019-10-24 12:45:15 +0200

    prueba CUsuario

--- controller/CUsuario.php
+++ controller/CUsuario.php
@@ -4,7 +4,7 @@ include_once ("../model/usuario_model.php");
 $usuarios= new usuario_model();
 $usuarios->setList();
 
-foreach ($usuarios->list as $object){
+foreach ($usuarios->getList() as $object){
     if($object->getUsuario()==$_GET["user"] && $object->getContrasena()==$_GET["pass"]){
         session_start();       
         $_SESSION["usuario"] = $object->getUsuario();
