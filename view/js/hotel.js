659d86136fc4310384fb4e3e8fc0865e8709e848
iker <iker98g@gmail.com>
2019-10-25 10:21:12 +0200

    hotel.js prueba17

--- view/js/hotel.js
+++ view/js/hotel.js
@@ -16,16 +16,14 @@ $( document ).ready(function() {
 			alert("hola3");
 
 			$.each(habitaciones,function(index,room){
-				/* newRow += "<div class='col-12 col-md-6 col-lg-4'>"
+				newRow += "<div class='col-12 col-md-6 col-lg-4'>"
 								+"<div class='card'></div>"
 									+"<div class='card-body'></div>"
 										+"<img src='"+room.imagen+"'</img>"
 										+"<h5 class='card-title'>"+room.tipo+"</h5>"
 										+"<p class='card-text'>"+room.precio+"</p>";
- */
-				newRow+="agur";
+ 
 			}); 
-			alert(newRow);
 			
 		},
 	})
