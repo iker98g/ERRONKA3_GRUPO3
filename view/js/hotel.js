c1fc7f68c7c08a6b8c6def829bf0f897e5fe05b1
iker <iker98g@gmail.com>
2019-10-25 10:12:24 +0200

    hotel.js prueba13

--- view/js/hotel.js
+++ view/js/hotel.js
@@ -16,13 +16,23 @@ $( document ).ready(function() {
 			alert("hola3");
 
 			$.each(habitaciones,function(index,room){
-				newRow += "<div class='col-12 col-md-6 col-lg-4'></div>"
+				/* newRow += "<div class='col-12 col-md-6 col-lg-4'>"
 								+"<div class='card'></div>"
 									+"<div class='card-body'></div>"
 										+"<img src='"+room.imagen+"'</img>"
 										+"<h5 class='card-title'>"+room.tipo+"</h5>"
 										+"<p class='card-text'>"+room.precio+"</p>";
-
+ */
+				newRow+="<div class='col-12 col-md-6 col-lg-4'>
+				<div class='card'>
+					<div class='card-body'>
+						<img src='img/estandar1.jpg' class='card-img-top' alt='...'>
+						<h5 class='card-title'>Estandar</h5>
+						<p class='card-text'>65€</p>
+						<a href='#' class='btn btn-primary'>Reservar</a>
+					</div>
+				</div>
+			</div>"
 			}); 
 			alert(newRow);
 			$("#habitas").append(newRow);
