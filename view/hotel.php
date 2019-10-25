b9b32dd6f0f56d966d528248b7ea14f6cb230e43
iker <iker98g@gmail.com>
2019-10-25 09:19:30 +0200

    hotel.js

--- view/hotel.php
+++ view/hotel.php
@@ -108,8 +108,8 @@
                 </div>
                 <div class="tab-pane fade" id="list-habitaciones" role="tabpanel" aria-labelledby="list-habitaciones-list">
                     <h5>Habitaciones</h5>
-                    <div class="row">
-                        <div class="col-12 col-md-6 col-lg-4">
+                    <div class="row" id="habitas">
+                        <!-- <div class="col-12 col-md-6 col-lg-4">
                             <div class="card">
                                 <div class="card-body">
                                     <img src="img/estandar1.jpg" class="card-img-top" alt="...">
@@ -118,7 +118,7 @@
                                     <a href="#" class="btn btn-primary">Reservar</a>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
                 <div class="tab-pane fade" id="list-ubicacion" role="tabpanel" aria-labelledby="list-ubicacion-list">
--- view/js/hotel.js
+++ view/js/hotel.js
@@ -10,6 +10,21 @@ $( document ).ready(function() {
 			alert(result);
 			var habitaciones=JSON.parse(result);
 			console.log(result);
+			var newRow="";
+
+			$.each(habitaciones,function(index,room){
+				newRow += "<div class='col-12 col-md-6 col-lg-4'></div"
+								+"<div class='card'></div>"
+									+"<div class='card-body'></div>"
+										+'<img src="'+room.imagen+'"</img>'
+										+"<h5 class='card-title'>"+room.tipo+"</h5>"
+										+"<p class='card-text'>"+room.precio+"</p>"
+
+			});
+			$("#habitas").append(newRow);
+		},
+		error : function(xhr) {
+			alert("An error occured: " + xhr.status + " " + xhr.statusText);
 		}
-	})
+	});
 });
