95292166c6ec55f66308b0a23fb08ec3dad99e76
iker <iker98g@gmail.com>
2019-10-29 10:05:04 +0100

    servicios.php dropdown prueba21

--- view/js/servicios.js
+++ view/js/servicios.js
@@ -1,18 +1,33 @@
 $( document ).ready(function() {
 
-	
-		  window.onclick = function(event) {
-			if (!event.target.matches('.dropbtn')) {
-			  var dropdowns = document.getElementsByClassName("dropdown-content");
-			  var i;
-			  for (i = 0; i < dropdowns.length; i++) {
+	window.onclick = function(event) {
+		if (!event.target.matches('.dropbtn')) {
+			var dropdowns = document.getElementsByClassName("dropdown-content");
+			var i;
+			for (i = 0; i < dropdowns.length; i++) {
 				var openDropdown = dropdowns[i];
 				if (openDropdown.classList.contains('show')) {
 				  openDropdown.classList.remove('show');
 				}
-			  }
 			}
-		  }
+		}
+	}
+
+	$('.cerrarSesion').click(function(){
+		$.ajax({
+		       	type: "GET",
+		       	url: "../controller/CCerrarSesion.php", 
+		       	datatype: "json",  //type of the result
+		       	success: function(result){  
+		       		
+		       		alert("Sesion cerrada");
+		       		window.location.replace(result); //recarga la pagina
+		       	},
+		       	error : function(xhr) {
+		   			alert("An error occured: " + xhr.status + " " + xhr.statusText);
+		   		}
+		});
+	});
 
 });
 	function myFunction() {
