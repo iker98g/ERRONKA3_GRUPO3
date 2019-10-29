a2401b3ba4ce8da9c9345d9ba9cfb34abfb1f4a9
iker <iker98g@gmail.com>
2019-10-29 09:30:01 +0100

    servicios.php dropdown prueba10

--- view/servicios.php
+++ view/servicios.php
@@ -75,6 +75,27 @@ session_start();
     </ul>
 </footer>
 <!-- FIN FOOTER -->
-<script src="js/servicios.js" type="text/javascript"></script>
+<!-- <script src="js/servicios.js" type="text/javascript"></script> -->
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
 </body>
 </html>
