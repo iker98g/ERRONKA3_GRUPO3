62ba32837077d9b99ec66c7f18d9388fdbd86ab0
ikaslea <ikaslea@ETHAZI23>
2019-10-30 13:08:07 +0100

    vuelta de las condiciones

--- model/reserva_model.php
+++ model/reserva_model.php
@@ -1,9 +1,9 @@
 <?php
 //Klase honetan gordetzen dira datu basearen datuak. erabiltzailea...
-if ($_SERVER['SERVER_NAME']=="http://tres.fpz1920.com/") {
-    include_once ("connect_data_remote.php");
-}else {
+if ($_SERVER['SERVER_NAME']=="localhost") {
     include_once ("connect_data_local.php");
+}else {
+    include_once ("connect_data_remote.php");
 }
 
 include_once ("reserva_class.php");
