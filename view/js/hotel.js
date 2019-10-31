40a4e16bbbee2de6a89506ff4438535ef092c8c5
deathblade <deathbladebass@gmail.com>
2019-10-31 10:01:16 +0100

    reservas prueb

--- view/js/hotel.js
+++ view/js/hotel.js
@@ -48,7 +48,12 @@ $( document ).ready(function() {
 	
 	$("#fechaInicio").change(function(){
 		var fechaInicio=$("#fechaInicio").val();
-		cambioReservas();
+		
+		$("#fechaFin").val("");
+		
+		$( "#tipo" ).slideUp( "slow");
+		$("select[name=tipoHabitacion]").val("elige");
+
 		var d = new Date();
 		var month = d.getMonth()+1;
 		var day = d.getDate();
@@ -76,12 +81,90 @@ $( document ).ready(function() {
 			$("#tipo").slideUp( "slow" );
 			$("select[name=tipoHabitacion]").val("elige");
 		}	
-		
-		
 	});
 
 	$("#fechaFin").change(function(){
-		cambioReservas();
+		var fechaInicio=$("#fechaInicio").val();
+		var fechaFin=$("#fechaFin").val();
+
+		if(fechaInicio > fechaFin) {
+			alert("Tal vez esa combinacion de días no sea buena idea, elige otra fecha final.");
+			$("#fechaFin").val("");
+			$("#precioTotal").fadeOut("slow");
+			$(".labelPrecio").fadeOut("slow");
+			$( "#tipo" ).slideUp( "slow");
+			$("select[name=tipoHabitacion]").val("elige");
+		}else if (fechaInicio == fechaFin){
+			alert("Reserva al menos para una noche.");
+			$("#fechaFin").val("");
+			$("#precioTotal").fadeOut("slow");
+			$(".labelPrecio").fadeOut("slow");
+			$("#tipo").slideUp( "slow");
+			$("select[name=tipoHabitacion]").val("elige");
+		}else {
+			if ($('#tipo').is(':visible')) {
+				calcularTotal(precioHabitacion);
+			}
+
+		  	$.ajax({
+		       	type: "GET",
+		       	data:{'fechaInicio':fechaInicio, 'fechaFin':fechaFin},
+		       	url: "../controller/cDisponibilidadReserva.php", 
+		       	datatype: "json",  //type of the result
+		       	success: function(result){  
+					if(fechaInicio < fechaFin) {
+						$( "#tipo" ).slideDown("slow");
+						
+						var habitacionesOcupadas = JSON.parse(result);
+	
+						console.log(habitacionesOcupadas);
+	
+						var countSuites = 0;
+						var countEstandares = 0;
+						var countSuperiores = 0;
+	
+						for(i=0; i<habitacionesOcupadas.length; i++) {
+							if(habitacionesOcupadas[i].idHabitacion > 0 && habitacionesOcupadas[i].idHabitacion < 5) {
+								countSuites++;
+								if(countSuites==4) {
+									$("#suite").hide();
+									console.log(countSuites);
+									countSuites = 0;
+									console.log(countSuites);
+								}
+							}else if(habitacionesOcupadas[i].idHabitacion > 4 && habitacionesOcupadas[i].idHabitacion < 9) {
+								countEstandares++;
+								console.log(countEstandares);
+								if(countEstandares==4) {
+									$("#estandar").attr('disabled','disabled');
+									console.log(countEstandares);
+									countEstandares = 0;
+									console.log(countEstandares);
+								}
+							}else if(habitacionesOcupadas[i].idHabitacion > 8 && habitacionesOcupadas[i].idHabitacion < 13) {
+								countSuperiores++;
+								console.log(countSuperiores);
+								if(countSuperiores==4) {
+									$("#superior").attr('disabled','disabled');
+									console.log(countSuperiores);
+									countSuperiores = 0;
+									console.log(countSuperiores);
+								}
+							}
+						}			
+					}
+	
+					$("select[name=tipoHabitacion]").change(function(){
+						var tipoHabitacion=$("select[name=tipoHabitacion]").val();
+						
+						callTipo(tipoHabitacion, habitacionesOcupadas);
+					});
+		       	},
+		       	error : function(xhr) {
+		   			alert("An error occured: " + xhr.status + " " + xhr.statusText);
+		   		}
+		    });
+		}
 	});
 
 	function callTipo(tipoHabitacion, habitacionesOcupadas) {
@@ -224,91 +307,3 @@ $( document ).ready(function() {
 		$(".labelPrecio").fadeIn("slow");
 	}		
 });
-
-
-
-function cambioReservas() {
-	var fechaInicio=$("#fechaInicio").val();
-	var fechaFin=$("#fechaFin").val();
-
-	if(fechaInicio > fechaFin) {
-		alert("Tal vez esa combinacion de días no sea buena idea, elige otra fecha final.");
-		$("#fechaFin").val("");
-		$("#precioTotal").fadeOut("slow");
-		$(".labelPrecio").fadeOut("slow");
-		$( "#tipo" ).slideUp( "slow");
-		$("select[name=tipoHabitacion]").val("elige");
-	}else if (fechaInicio == fechaFin){
-		alert("Reserva al menos para una noche.");
-		$("#fechaFin").val("");
-		$("#precioTotal").fadeOut("slow");
-		$(".labelPrecio").fadeOut("slow");
-		$("#tipo").slideUp( "slow");
-		$("select[name=tipoHabitacion]").val("elige");
-	}else {
-		if ($('#tipo').is(':visible')) {
-			calcularTotal(precioHabitacion);
-		}
-
-	  	$.ajax({
-	       	type: "GET",
-	       	data:{'fechaInicio':fechaInicio, 'fechaFin':fechaFin},
-	       	url: "../controller/cDisponibilidadReserva.php", 
-	       	datatype: "json",  //type of the result
-	       	success: function(result){  
-				if(fechaInicio < fechaFin) {
-					$( "#tipo" ).slideDown("slow");
-					
-					var habitacionesOcupadas = JSON.parse(result);
-
-					console.log(habitacionesOcupadas);
-
-					var countSuites = 0;
-					var countEstandares = 0;
-					var countSuperiores = 0;
-
-					for(i=0; i<habitacionesOcupadas.length; i++) {
-						if(habitacionesOcupadas[i].idHabitacion > 0 && habitacionesOcupadas[i].idHabitacion < 5) {
-							countSuites++;
-							if(countSuites==4) {
-								$("#suite").hide();
-								console.log(countSuites);
-								countSuites = 0;
-								console.log(countSuites);
-							}
-						}else if(habitacionesOcupadas[i].idHabitacion > 4 && habitacionesOcupadas[i].idHabitacion < 9) {
-							countEstandares++;
-							console.log(countEstandares);
-							if(countEstandares==4) {
-								$("#estandar").attr('disabled','disabled');
-								console.log(countEstandares);
-								countEstandares = 0;
-								console.log(countEstandares);
-							}
-						}else if(habitacionesOcupadas[i].idHabitacion > 8 && habitacionesOcupadas[i].idHabitacion < 13) {
-							countSuperiores++;
-							console.log(countSuperiores);
-							if(countSuperiores==4) {
-								$("#superior").attr('disabled','disabled');
-								console.log(countSuperiores);
-								countSuperiores = 0;
-								console.log(countSuperiores);
-							}
-						}
-					}			
-				}
-
-				$("select[name=tipoHabitacion]").change(function(){
-					var tipoHabitacion=$("select[name=tipoHabitacion]").val();
-					
-					callTipo(tipoHabitacion, habitacionesOcupadas);
-				});
-	       	},
-	       	error : function(xhr) {
-	   			alert("An error occured: " + xhr.status + " " + xhr.statusText);
-	   		}
-	    });
-	}
-}
-	
-
