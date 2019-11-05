d90c14f237a11455bc3561a406537cc182b2ee3b
deathblade <deathbladebass@gmail.com>
2019-11-05 09:31:22 +0100

    fix

--- view/vReservas.php
+++ view/vReservas.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if ($_SESSION["admin"]==null) {
+if ($_SESSION["admin"] == null) {
     header("Location: http://tres.fpz1920.com/index.php");
     die();
 }
@@ -10,78 +10,82 @@ if ($_SESSION["admin"]==null) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hotel LES</title>
-    <link rel="icon" type="image/png" href="img/favicon.ico">
-    <script src="https://kit.fontawesome.com/661afcc94b.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="css/admin.css" rel="stylesheet" type="text/css"/>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/100/three.min.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Hotel LES</title>
+<link rel="icon" type="image/png" href="img/favicon.ico">
+<script src="https://kit.fontawesome.com/661afcc94b.js"
+	crossorigin="anonymous"></script>
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+	crossorigin="anonymous">
+<link href="css/admin.css" rel="stylesheet" type="text/css" />
+
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/three.js/100/three.min.js"></script>
 <script src="https://www.vantajs.com/dist/vanta.net.min.js"></script>
 </head>
 <body>
-<div id="vantaBG" class="fixed-bottom"></div>
-<!-- HEADER -->
-<header> 
-    <!-- NAV -->
-    <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Hotel LES</a>
+	<div id="vantaBG" class="fixed-bottom"></div>
+	<!-- HEADER -->
+	<header>
+		<!-- NAV -->
+		<nav class="navbar navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">Hotel LES</a>
 
-        <div class="dropdown dropleft">
-            <button class="btn btn-light" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <a id="nombreUsuario" data-id="<?php echo $_SESSION["id"]; ?>"><?php echo $_SESSION["usuario"]; ?></a>
-                <i class="far fa-user-circle fa-lg"></i>
-            </button>
-           <?php 
-           if ($_SESSION["admin"]==1) {
-              ?>    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="vAdmin.php">
-                    <i class="fas fa-users-cog"></i>
-						Panel Admin
-				</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="vServicios.php">
-                    <i class="fas fa-concierge-bell"></i>
-						Servicios
-				</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item cerrarSesion" href="javascript:void(0);">
-                    <i class="fas fa-sign-out-alt"></i>    
-                    Cerrar sesión
-                </a>
-                
-            </div><?php ;
-           }else{
-           
-           
-           ?>
-                   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="vServicios.php">
-                    <i class="fas fa-concierge-bell"></i>
-						Servicios
-				</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item cerrarSesion" href="javascript:void(0);">
-                    <i class="fas fa-sign-out-alt"></i>    
-                    Cerrar sesión
-                </a>
-                
-            </div>
+			<div class="dropdown dropleft">
+				<button class="btn btn-light" type="button" id="dropdownMenuButton"
+					data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<a id="nombreUsuario" data-id="<?php echo $_SESSION["id"]; ?>"><?php echo $_SESSION["usuario"]; ?></a>
+					<i class="far fa-user-circle fa-lg"></i>
+				</button>
+           <?php
+        if ($_SESSION["admin"] == 1) {
+            ?>    <div class="dropdown-menu"
+					aria-labelledby="dropdownMenuButton">
+					<a class="dropdown-item" href="vAdmin.php"> <i
+						class="fas fa-users-cog"></i> Panel Admin
+					</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="vServicios.php"> <i
+						class="fas fa-concierge-bell"></i> Servicios
+					</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item cerrarSesion" href="javascript:void(0);"> <i
+						class="fas fa-sign-out-alt"></i> Cerrar sesión
+					</a>
+
+				</div><?php
+
+;
+        } else {
+
+            ?>
+                   <div class="dropdown-menu"
+					aria-labelledby="dropdownMenuButton">
+					<a class="dropdown-item" href="vServicios.php"> <i
+						class="fas fa-concierge-bell"></i> Servicios
+					</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item cerrarSesion" href="javascript:void(0);"> <i
+						class="fas fa-sign-out-alt"></i> Cerrar sesión
+					</a>
+
+				</div>
                     
                     <?php }?>
             
         </div>
-    </nav>
-    <!-- FIN NAV -->
-</header> 
-<!-- FIN HEADER -->     
-<!-- MAIN -->
-		<main>
-		<div class="container mb-5">
-<h1 class="text-center display-3 tituloWeb mt-5">MIS RESERVAS</h1>
+		</nav>
+		<!-- FIN NAV -->
+	</header>
+	<!-- FIN HEADER -->
+	<!-- MAIN -->
+	<main>
+	<div class="container mb-5">
+		<h1 class="text-center display-3 tituloWeb mt-5">MIS RESERVAS</h1>
 		<div class="table-responsive mb-5">
 			<table class="table table-bordered mt-5 ">
 				<thead class="thead-dark">
@@ -99,26 +103,43 @@ if ($_SESSION["admin"]==null) {
 				</tbody>
 			</table>
 		</div>
-</div>
-		</main>
-		<!-- FIN MAIN -->    
-<!-- FOOTER -->
-<footer class="text-muted text-center text-small fixed-bottom border-top bg-white shadow">
-    <p class="mb-1">&copy; Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla maiores ipsam in inventore voluptatibus. Labore, provident. Quos ratione accusantium facere? Rem assumenda, a adipisci possimus repudiandae ipsam mollitia debitis architecto.</p>
-    <ul class="list-inline">
-        <li class="list-inline-item"><a href="https://www.instagram.com/?hl=es"><i class="fab fa-instagram"></i></a></li>
-        <li class="list-inline-item"><a href="https://es-es.facebook.com/"><i class="fab fa-facebook-square"></i></a></li>
-        <li class="list-inline-item"><a href="https://twitter.com/?lang=es"><i class="fab fa-twitter"></i></a></li>
-        <li class="list-inline-item"><a href="https://support.google.com/plus/?hl=es-419#topic=9259565"><i class="fab fa-google-plus-g"></i></a></li>
-    </ul>
-</footer>
-<!-- FIN FOOTER -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>    
-<script src="js/reserva.js" type="text/javascript"></script>
+	</div>
+	</main>
+	<!-- FIN MAIN -->
 
-<script>
+	<br>
+	<br>
+	<br>
+	<br>
+	<!-- FOOTER -->
+	<footer
+		class="text-muted text-center text-small fixed-bottom border-top bg-white shadow">
+		<p class="mb-1">&copy; Lorem ipsum dolor sit amet consectetur
+			adipisicing elit. Nulla maiores ipsam in inventore voluptatibus.
+			Labore, provident. Quos ratione accusantium facere? Rem assumenda, a
+			adipisci possimus repudiandae ipsam mollitia debitis architecto.</p>
+		<ul class="list-inline">
+			<li class="list-inline-item"><a
+				href="https://www.instagram.com/?hl=es"><i class="fab fa-instagram"></i></a></li>
+			<li class="list-inline-item"><a href="https://es-es.facebook.com/"><i
+					class="fab fa-facebook-square"></i></a></li>
+			<li class="list-inline-item"><a href="https://twitter.com/?lang=es"><i
+					class="fab fa-twitter"></i></a></li>
+			<li class="list-inline-item"><a
+				href="https://support.google.com/plus/?hl=es-419#topic=9259565"><i
+					class="fab fa-google-plus-g"></i></a></li>
+		</ul>
+	</footer>
+	<!-- FIN FOOTER -->
+	<script
+		src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script
+		src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script src="js/reserva.js" type="text/javascript"></script>
+
+	<script>
         VANTA.NET({
           el: "#vantaBG",
           color: 0x222222,
